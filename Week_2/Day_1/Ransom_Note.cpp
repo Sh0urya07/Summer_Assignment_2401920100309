994fda822dc05556c650be9ee064431da7612338
Sh0urya07 <pratapshourya07@gmail.com>
2026-06-08 22:41:19 +0530

    Create Ransom_Note.cpp

---
+++ Week_2/Day_1/Ransom_Note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[26] = {0};
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine[i] - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            count[ransomNote[i] - 'a']--;
+            if (count[ransomNote[i] - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
